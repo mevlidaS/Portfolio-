380c25f3f611b2d918578f641011a16e46c6440b
Mevlida Salendrkovic <mevlida.salendrkovic@gmail.com>
2024-06-20 16:17:17 +0200

    responsive

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
