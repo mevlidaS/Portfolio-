579279c1fd8aa13dae30132b29b7433cdcad2e43
Mevlida Salendrkovic <mevlida.salendrkovic@gmail.com>
2024-06-21 11:07:32 +0200

    burgermenu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,45 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { TranslationService } from '../../services/translation.service';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-translate = inject(TranslationService);
+  isScreenSmall: boolean = false;
+  open = false;
 
+  translate = inject(TranslationService);
+
+  checkSize() {
+    if (typeof window !== 'undefined') {
+      this.isScreenSmall = window.innerWidth <= 1180;
+    }
+  }
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.checkSize();
+  }
+
+  toggleMenu() {
+    this.open = !this.open;
+  }
+
+  closeMenu() {
+    this.open = false;
+  }
 }
